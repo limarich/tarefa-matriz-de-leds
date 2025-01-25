e908375c2ead439672dc4c87f41716bda7512fd6
Richard <richard.esclima@gmail.com>
2025-01-25 12:32:51 -0300

    feat: cria animação de sorriso

--- matriz-de-leds.c
+++ matriz-de-leds.c
@@ -24,6 +24,18 @@ static void gpio_irq_handler(uint gpio, uint32_t events)
     reset_usb_boot(0, 0); // habilita o modo de gravação do microcontrolador
 }
 
+// CORES PARA AS ANIMAÇÕES
+float intensity = 1.0f;
+
+const pixel black = {0, 0, 0};
+const pixel white = {213, 210, 210};
+const pixel green = {32, 216, 26};
+const pixel dark_green = {6, 80, 36};
+const pixel brown = {181, 68, 81};
+const pixel red = {252, 6, 6};
+
+// ANIMAÇÕES
+void makeSmile(PIO pio, uint sm);
 // função principal
 int main()
 {
@@ -66,6 +78,188 @@ int main()
     while (true)
     {
         printf("\nfrequência de clock %ld\r\n", clock_get_hz(clk_sys));
+        makeSmile(pio, sm);
         sleep_ms(1000);
+    }
+}
+void makeSmile(PIO pio, uint sm)
+{
+    frame frames[5] = {
+        {
+            // linha 1
+            dark_green,
+            dark_green,
+            dark_green,
+            dark_green,
+            dark_green,
+            //     // linha 2
+            dark_green,
+            black,
+            black,
+            black,
+            dark_green,
+            //     // linha 3
+            black,
+            black,
+            black,
+            black,
+            black,
+            //     // linha 4
+            black,
+            dark_green,
+            black,
+            dark_green,
+            black,
+            //     // linha 5
+            black,
+            dark_green,
+            black,
+            dark_green,
+            black,
+        },
+        {
+            // linha 1
+            dark_green,
+            dark_green,
+            dark_green,
+            dark_green,
+            dark_green,
+            //     // linha 2
+            dark_green,
+            black,
+            black,
+            black,
+            dark_green,
+            //     // linha 3
+            black,
+            black,
+            black,
+            black,
+            black,
+            //     // linha 4
+            black,
+            dark_green,
+            black,
+            black,
+            black,
+            //     // linha 5
+            black,
+            black,
+            black,
+            dark_green,
+            black,
+        },
+        {
+            // linha 1
+            black,
+            dark_green,
+            dark_green,
+            dark_green,
+            black,
+            //     // linha 2
+            dark_green,
+            dark_green,
+            dark_green,
+            dark_green,
+            dark_green,
+            //     // linha 3
+            black,
+            black,
+            black,
+            black,
+            black,
+            //     // linha 4
+            black,
+            dark_green,
+            black,
+            dark_green,
+            black,
+            //     // linha 5
+            black,
+            dark_green,
+            black,
+            dark_green,
+            black,
+        },
+        {
+            // linha 1
+            black,
+            black,
+            black,
+            black,
+            black,
+            //     // linha 2
+            black,
+            dark_green,
+            dark_green,
+            dark_green,
+            black,
+            //     // linha 3
+            black,
+            black,
+            black,
+            black,
+            black,
+            //     // linha 4
+            black,
+            dark_green,
+            black,
+            dark_green,
+            black,
+            //     // linha 5
+            black,
+            dark_green,
+            black,
+            dark_green,
+            black,
+        },
+        {
+            // linha 1
+            black,
+            black,
+            red,
+            black,
+            black,
+            //     // linha 2
+            black,
+            dark_green,
+            red,
+            dark_green,
+            black,
+            //     // linha 3
+            black,
+            black,
+            black,
+            black,
+            black,
+            //     // linha 4
+            black,
+            dark_green,
+            black,
+            dark_green,
+            black,
+            //     // linha 5
+            black,
+            dark_green,
+            black,
+            dark_green,
+            black,
+        },
+    };
+    for (uint i = 0; i < 5; i++)
+    {
+        draw_pio(frames[i], pio, sm, intensity);
+        if (i != 4)
+        {
+            play_tone(BUZZER_A, 440, 250);
+        }
+        else
+        {
+
+            play_tone(BUZZER_A, 100, 100);
+            sleep_ms(100);
+            play_tone(BUZZER_A, 50, 100);
+        }
+        sleep_ms(500);
     }
 }
