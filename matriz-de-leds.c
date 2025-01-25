6eb56c9eab52b206e82014c128cb3b607f3fe2cf
Richard <richard.esclima@gmail.com>
2025-01-25 10:43:17 -0300

    feat: adiciona configuracao inicial do projeto

---
+++ matriz-de-leds.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "hardware/adc.h"
+#include "pico/bootrom.h"
+#include "hardware/pwm.h"
+#include "libs/buzzer.h"
+#include "libs/leds.h"
+#include "pio_matrix.pio.h"
+
+// botão de interupção
+const uint button_0 = 5;
+const uint button_1 = 6;
+
+// rotina da interrupção
+static void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    printf("Interrupção ocorreu no pino %d, no evento %d\n", gpio, events);
+    printf("HABILITANDO O MODO GRAVAÇÃO");
+    reset_usb_boot(0, 0); // habilita o modo de gravação do microcontrolador
+}
+
+// função principal
+int main()
+{
+    PIO pio = pio0;
+    bool ok;
+    uint16_t i;
+    double r = 0.0, b = 0.0, g = 0.0;
+    frame draw;
+
+    // coloca a frequência de clock para 128 MHz, facilitando a divisão pelo clock
+    ok = set_sys_clock_khz(128000, false);
+
+    // Inicializa todos os códigos stdio padrão que estão ligados ao binário.
+    stdio_init_all();
+
+    printf("iniciando a transmissão PIO");
+    if (ok)
+        printf("clock set to %ld\n", clock_get_hz(clk_sys));
+
+    // configurações da PIO
+    uint offset = pio_add_program(pio, &pio_matrix_program);
+    uint sm = pio_claim_unused_sm(pio, true);
+    pio_matrix_program_init(pio, sm, offset, OUT_PIN);
+
+    // inicializar o botão de interrupção - GPIO5
+    gpio_init(button_0);
+    gpio_set_dir(button_0, GPIO_IN);
+    gpio_pull_up(button_0);
+
+    // inicializar o botão de interrupção - GPIO5
+    gpio_init(button_1);
+    gpio_set_dir(button_1, GPIO_IN);
+    gpio_pull_up(button_1);
+
+    // interrupção da gpio habilitada
+    gpio_set_irq_enabled_with_callback(button_0, GPIO_IRQ_EDGE_FALL, 1, &gpio_irq_handler);
+
+    while (true)
+    {
+        printf("\nfrequência de clock %ld\r\n", clock_get_hz(clk_sys));
+        sleep_ms(1000);
+    }
+}
